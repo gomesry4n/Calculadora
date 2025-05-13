d01772766a49a8f54439cbe58bf8690f3ef5e706
gomesry4n <ryan.g.silva19@aluno.senai.br>
2025-05-13 11:13:17 -0300

    first commit

---
+++ java.js
@@ -0,0 +1,17 @@
+function calcular(conta) {
+  let v1 = parseFloat(document.getElementById("valor1").value);
+  let v2 = parseFloat(document.getElementById("valor2").value);
+  let result = 0;
+
+  if (conta == "+") {
+    result = v1 + v2;
+  } else if (conta == "-") {
+    result = v1 - v2;
+  } else if (conta == "*") {
+    result = v1 * v2;
+  } else if (conta == "/") {
+    result = v1 / v2;
+  }
+
+  document.getElementById("resultado").value = result;
+}
